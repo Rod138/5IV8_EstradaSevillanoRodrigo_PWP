7fd2b3c14b8473843a0c95959a1956da7b5a33a8
Rod138 <restradas2301@alumno.ipn.mx>
2025-10-08 18:02:20 -0600

    Crear 05Ejercicios

---
+++ 05Ejercicios/js/funciones1.js
@@ -0,0 +1,27 @@
+function validarn(e){
+    var teclado = (document.all)? e.keyCode : e.which;
+    if (teclado == 8) return true;
+    var patron = /[0-9\d .]/;
+
+    var codigo = String.fromCharCode(teclado);
+    return patron.test(codigo);
+}
+
+//Función para calcular el interés
+//Delimitar el número de decimales
+function interes(){
+    var valor = document.getElementById("cantidadi").value;
+
+    var parseo = parseFloat(valor);
+    alert(parseo)
+    var interes = parseo*0.085; //Límite a 2 decimales
+    alert(interes)
+    var total = interes + parseo;
+    alert(total)
+    document.getElementById("saldoi").value = "$ " + total; //Límite a 2 decimales
+}
+
+function borrari(){
+    document.getElementById("cantidadi").value = "";
+    document.getElementById("saldoi").value = "";
+}
