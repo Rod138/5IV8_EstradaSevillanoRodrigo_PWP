29ad6d2454f3433f6bf71f5c505f3077f98d5d19
Rod138 <restradas2301@alumno.ipn.mx>
2025-10-22 19:04:37 -0600

    Corregir variable servidor (09Node)

--- 09Node/index.js
+++ 09Node/index.js
@@ -1,11 +1,10 @@
-const { response } = require('express');
 var http = require('http');
 
 //Vamos a crear nuestro propio servidor
-var servidor = http.createServer(function (pedido, respuesta) {
+var servidor = http.createServer(function (req, res) {
     //Req -> request es una solicitud, viene por parte de la arquitectura cliente-servidor, todos los clientes (navegadores, usuarios, app, servicios, etc), son los que realizan una petición por parte del protocolo
     //Res -> responese es la respuesta que el servidor le da al cliente
-    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8'});
+    res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8'});
     res.write("<h1>Hola mundo desde Node.js</h1>");
     res.write("<h1>A dormir</h1>");
     console.log("Hola, si entró al servidor");
